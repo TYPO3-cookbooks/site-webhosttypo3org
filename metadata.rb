d82c0c9822a327676197c82c42154e661655b316
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-16 13:26:58 +0100

    Initial version

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             "site-webhosttypo3org"
+maintainer       "Steffen Gebert"
+maintainer_email "steffen.gebert@typo3.org"
+license          "Apache 2.0"
+description      "Additions to the Puppet-based web hosting setup"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "1.0.0"
+
+supports         "debian"
+
+# base cookbook
+depends          "t3-base",     "~> 0.2.0"
