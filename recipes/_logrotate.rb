8dabddc43bce4fd7167094f9e97b15260f52f1ec
Andri Steiner <andri@snowflakeops.ch>
2017-01-17 23:59:18 +0100

    [TASK] specify owner, add nocreate

--- recipes/_logrotate.rb
+++ recipes/_logrotate.rb
@@ -7,5 +7,7 @@ Logrotate configuration
 logrotate_app "web-logs" do
   path "/home/*/log/*.log"
   frequency 'daily'
+  su 'root root'
+  options [ 'nocreate', 'missingok', 'compress', 'delaycompress', 'notifempty', 'sharedscripts' ]
   postrotate '[ ! -f /var/run/nginx.pid ] || kill -USR1 $(cat /var/run/nginx.pid)'
 end
